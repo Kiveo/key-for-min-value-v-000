900fa4622860a0bd17992fbf94a9e9c5b0862166
Maverick Tesini <mavericktesini@gmail.com>
2018-03-12 15:31:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   reference = "x"
   vref = 1000
   name_hash.collect do |key, value|
-    key = reference
+    reference = key
     if value < vref
       key
     end
