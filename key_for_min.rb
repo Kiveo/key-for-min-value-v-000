c1d09cbf43a7b47edb486df3024b8605d8b94033
Maverick Tesini <mavericktesini@gmail.com>
2018-03-12 15:33:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  reference = "x"
+  reference = nil
   vref = 1000
   name_hash.collect do |key, value|
-
     if value < vref
       reference = key
     end
