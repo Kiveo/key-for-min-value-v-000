938a62c3f4fa16fcf58bedf3fb74163661e3b87f
Maverick Tesini <mavericktesini@gmail.com>
2018-03-12 15:22:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   baseline = 1000
   name_hash.collect do |key, value|
     if value == nil
-      nil
+      return nil
     elsif value < baseline
       key
     end
